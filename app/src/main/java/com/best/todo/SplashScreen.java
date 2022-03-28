ed4ecfe8f0d7d730c10d2cbecdc39c910ea94d6c
culty48 <robcultraro@gmail.com>
2022-03-28 16:00:52 -0400

    Made it so that splash screen displays on startup (needs light/night mode configuration)

---
+++ app/src/main/java/com/best/todo/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.best.todo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
